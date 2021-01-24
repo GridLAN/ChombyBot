f3c273e1cf321ec316f1287dbcfe8777b62d99d1
deadshell <deadshell@gridlan.com>
2021-01-24 13:06:15 -0500

    added logging

--- src/main.py
+++ src/main.py
@@ -1,7 +1,16 @@
 import os
+import logging
 import asyncio
 import discord
 from discord.ext import commands
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler(
+    filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter(
+    '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 class Dog(commands.Cog):
     def __init__(self, bot):
